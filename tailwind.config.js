d4d3d4888d461311f185d02bb262a66765b27a46
0xcii <antcave.club@gmail.com>
2023-10-06 02:40:59 +0800

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,18 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      screens: {
+        "5xl": "2000px"
+      },
       fontFamily: {
-        coiny: ['Coiny', ...defaultTheme.fontFamily.sans]
-      },
-      colors: {
-        'brand-purple': 'var(--clr-purple)',
-        'brand-pink': 'var(--clr-pink)',
-        'brand-yellow': 'var(--clr-yellow)',
-        'brand-blue': 'var(--clr-blue)',
-        'brand-green': 'var(--clr-green)',
-        'brand-light': 'var(--clr-light)',
-        'brand-background': 'var(--clr-background)'
-      },
-      animation: {
-        'pulse-slow': 'pulse 10s linear infinite'
+        kanit: ["Kanit", "sans-serif"]
       }
-    }
+    },
   },
-  plugins: []
+  plugins: [],
 }
